6722ff71288a03b6bf90ac4c3bb684b60505d1a6
inimitable <anonymous@none.org>
2018-09-04 16:50:17 -0400

    Fix for division-by-zero error in angle computation

--- pygeometer/Vector.py
+++ pygeometer/Vector.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-"""docstring"""
+"""Vectors, including special case 3D and 2D vectors."""
+
 from math import acos
 
 from pygeometer.Pointlike import _PointlikeND
@@ -14,9 +15,6 @@ class VectorND(_PointlikeND):
     def magnitude(self):
         return sum(map(lambda x: x ** 2, self.coords)) ** 0.5
 
-    def __mul__(self, other: Number):
-        return self.scalar(other)
-
     def scalar(self, other: Number):
         """Computes the scalar multiple of this vector."""
         cls = self.__class__
@@ -27,7 +25,14 @@ class VectorND(_PointlikeND):
         assert self.dimension == other.dimension
         return sum(self[x] * other[x] for x in range(self.dimension))
 
+    def angle(self, other: 'VectorND'):
+        """Computes the angle between two vectors."""
+        if self.magnitude == 0 or other.magnitude == 0:
+            raise ValueError('cannot find angle between null vector and anything else')
+        return acos(((self.dot(other)) / self.magnitude) / other.magnitude)
 
+    def __mul__(self, other: Number):
+        return self.scalar(other)
 
 
 class Vector3D(VectorND):
@@ -42,7 +47,3 @@ class Vector3D(VectorND):
                 self[0] * other[1] - other[0] * self[1]
             )
         )
-
-    def angle(self, other: 'Vector3D'):
-        """Computes the angle between two vectors."""
-        return acos(((self.dot(other)) / self.magnitude) / other.magnitude)
